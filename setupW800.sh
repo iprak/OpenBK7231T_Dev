e422ad7f04f08efd7f4aa23a3c8ed43533e38961
Indu Prakash <leolite1@gmail.com>
2022-08-07 12:18:14 +0000

    W800

---
+++ setupW800.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd /workspaces/OpenBK7231T_Dev/OpenBK7231T_App/sdk
+
+echo "Updating submodule OpenW800"
+git submodule update OpenW800
+
+echo "Linking to /workspaces/OpenBK7231T_Dev/OpenBK7231T_App"
+mkdir OpenW800/sharedAppContainer
+ln -s /workspaces/OpenBK7231T_Dev/OpenBK7231T_App/ OpenW800/sharedAppContainer/sharedApp
